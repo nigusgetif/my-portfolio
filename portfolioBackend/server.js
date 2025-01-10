d0f43847aea990217dd1cca8b8dd8dfdf824291c
nigusgetif <nigusgetif48@gmail.com>
2025-01-10 16:03:40 +0300

    Add portfolioBackend folder with necessary files

---
+++ portfolioBackend/server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const cors = require('cors'); // Import the cors package
+
+const app = express();
+const port = 8080;
+
+app.use(cors({
+  origin: 'http://localhost:4200',
+  methods: ['POST'],
+  allowedHeaders: ['Content-Type'],
+}));
+
+app.use(bodyParser.json());
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'nigusgetif48@gmail.com',
+    pass: 'ibke vrtc ebde hjbh',
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+app.post('/api/send-email', (req, res) => {
+  const { name, email, message } = req.body;
+
+  const mailOptions = {
+    from: email,
+    to: 'nigusgetif48@gmail.com',
+    subject: `Message from ${name}`,
+    text: `You have received a new message from: \n\nName: ${name}\nEmail: ${email}\n\nMessage:\n${message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log('Error sending email:', error);
+      return res.status(500).json({ message: 'Failed to send email' });
+    }
+    console.log('Email sent:', info.response);
+    return res.status(200).json({ message: 'Email sent successfully!' });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
